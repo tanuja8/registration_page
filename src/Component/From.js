4d62e96f8373dc15adc5e72539f0509eff5acbe9
Tanuja <tanujakhatun292@gmail.com>
2023-06-17 13:26:03 +0530

    project updated

--- src/Component/From.js
+++ src/Component/From.js
@@ -1,15 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function From() {
     const data={name:"",email:"",password:""}
     const[inputData,setInputData]=useState(data);
+    const [flag,setFlag]= useState(false)
+    useEffect(()=>{
+
+    },[flag])
+    console.log("Registered")
     function handleData(e){
         setInputData({...inputData,[e.target.name]:e.target.value});
         console.log(inputData)
 }
+function handleSubmit(e){
+  e.preventDefault();
+  if(!inputData.name || !inputData.email || inputData.password)
+  {
+    alert("All Fields are Mendotary")
+  }
+  else{
+    setFlag(true)
+  }
+}
   return (
     <div>
-      <form className='container'>
+      <pre>{(flag)?<h2>Hello {inputData.name}, you've  registered successfully</h2>:""}</pre>
+      <form className='container' onSubmit={handleSubmit}>
         <div className='header'>
             <h1>Registration From</h1>
         </div>
@@ -18,7 +34,7 @@ export default function From() {
             onChange={handleData}></input>
         </div>
         <div>
-            <input type='text' placeholder='Enter your Email' name='Emai' value={inputData.email}  onChange={handleData}></input>
+            <input type='text' placeholder='Enter your Email' name='emai' value={inputData.email}  onChange={handleData}></input>
         </div>
         <div>
             <input type='text' placeholder='Enter your Password' name='password' value={inputData.password} onChange={handleData}></input>
